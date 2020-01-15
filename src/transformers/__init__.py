d54d984037325e1253f353a122d00cf74cd4fd29
gorjanradevski <gorjanradevski@gmail.com>
2020-01-15 13:14:12 +0100

    mlm head in bert

--- src/transformers/__init__.py
+++ src/transformers/__init__.py
@@ -152,6 +152,7 @@ if is_torch_available():
         BertForQuestionAnswering,
         load_tf_weights_in_bert,
         BERT_PRETRAINED_MODEL_ARCHIVE_MAP,
+        BertOnlyMLMHead
     )
     from .modeling_openai import (
         OpenAIGPTPreTrainedModel,
