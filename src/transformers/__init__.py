3da674c7681cf91bc85fb02e789c935fa042070b
Gorjan Radevski <gorjanradevski@gmail.com>
2020-06-19 14:43:30 +0200

    bug fix

--- src/transformers/__init__.py
+++ src/transformers/__init__.py
@@ -200,8 +200,7 @@ if is_torch_available():
         BertForTokenClassification,
         BertForQuestionAnswering,
         load_tf_weights_in_bert,
-        BERT_PRETRAINED_MODEL_ARCHIVE_MAP,
-        BertOnlyMLMHead
+        BertOnlyMLMHead,
         BERT_PRETRAINED_MODEL_ARCHIVE_LIST,
         BertLayer,
     )
